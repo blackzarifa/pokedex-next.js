df442f76e5a6a7891a0cff5569b07f5c8e2de962
blackzarifa <rafaelirades@gmail.com>
2024-10-06 15:08:36 -0300

    add: shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 import { useTranslations } from 'next-intl';
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   const t = useTranslations('Home');
 
-  return <h1>{t('title')}</h1>;
+  return (
+    <div>
+      <h1>{t('title')}</h1>
+      <Button>Test!</Button>
+    </div>
+  );
 }
