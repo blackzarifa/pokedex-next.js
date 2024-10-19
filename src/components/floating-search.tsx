42407a7452c097b08a2f2db809b416d5aa870176
blackzarifa <rafaelirades@gmail.com>
2024-10-19 19:28:12 -0300

    feat: floating search btn

---
+++ src/components/floating-search.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useTranslations } from 'next-intl';
+import { Button } from '@/components/ui/button';
+import { Search } from 'lucide-react';
+
+export default function FloatingSearch({ onClick }: { onClick: () => void }) {
+  const t = useTranslations('Options');
+
+  return (
+    <Button
+      className="fixed bottom-4 md:top-44 right-4 rounded-2xl h-14 w-14 shadow-lg"
+      onClick={onClick}
+    >
+      <Search />
+      <span className="sr-only">{t('search')}</span>
+    </Button>
+  );
+}
