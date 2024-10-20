324a3b5b076bd4944fd07f0e3f63a1f1821ad379
blackzarifa <rafaelirades@gmail.com>
2024-10-19 21:52:42 -0300

    refac: main page path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,5 @@
-import { useTranslations } from 'next-intl';
-import PokedexSearch from '@/components/pokedex-search';
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  const t = useTranslations('Home');
-
-  const arr = Array(100).fill(0);
-
-  return (
-    <>
-      <PokedexSearch />
-      <div>
-        {arr.map((x) => (
-          <h1>{x}</h1>
-        ))}
-      </div>
-      <h1>{t('title')}</h1>
-    </>
-  );
+  redirect('/pokemon');
 }
