c2a2a4a25a711eb3cad5df966123c411d4bff5f2
blackzarifa <rafaelirades@gmail.com>
2024-10-23 22:12:23 -0300

    feat: fetchPokemonDetails

--- src/app/api/pokemon.ts
+++ src/app/api/pokemon.ts
@@ -1,4 +1,4 @@
-import { PokemonListResponse } from '@/types/pokemon';
+import { PokemonListResponse, PokemonDetails } from '@/types/pokemon';
 
 const API_BASE_URL = 'https://pokeapi.co/api/v2';
 
@@ -7,8 +7,14 @@ export async function fetchPokemonList(
   offset: unknown = 0
 ): Promise<PokemonListResponse> {
   const response = await fetch(`${API_BASE_URL}/pokemon?limit=${limit}&offset=${offset}`);
-
   if (!response.ok) throw new Error('Failed to fetch list');
+
+  return response.json();
+}
+
+export async function fetchPokemonDetails(url: string): Promise<PokemonDetails> {
+  const response = await fetch(url);
+  if (!response.ok) throw new Error('Failed to fetch Pokémon details');
 
   return response.json();
 }
