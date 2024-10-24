2ef0108912ca8f11dcbb05f83b28f0e1afca8b7a
blackzarifa <rafaelirades@gmail.com>
2024-10-23 22:02:03 -0300

    feat: types for details

--- src/types/pokemon.ts
+++ src/types/pokemon.ts
@@ -9,3 +9,35 @@ export interface PokemonListResponse {
   previous: string | null;
   results: Pokemon[];
 }
+
+export interface PokemonDetails {
+  id: number;
+  name: string;
+  types: {
+    slot: number;
+    type: PokemonType;
+  };
+  sprites: PokemonSprites;
+}
+
+interface PokemonType {
+  name: string;
+  url: string;
+}
+
+interface PokemonSprites {
+  front_default: string;
+  front_shiny: string;
+  front_female: string;
+  front_shiny_female: string;
+  back_default: string;
+  back_shiny: string;
+  back_female: string;
+  back_shiny_female: string;
+  other: {
+    'official-artwork': {
+      front_default: string;
+      front_shiny: string;
+    };
+  };
+}
