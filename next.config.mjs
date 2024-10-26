6450687db71232e43b19a203e169139559ea5dd1
blackzarifa <rafaelirades@gmail.com>
2024-10-26 19:28:15 -0300

    feat: pokemon images

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,17 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        port: '',
+        pathname: '/PokeAPI/sprites/**',
+      },
+    ],
+  },
+};
 
 export default withNextIntl(nextConfig);
--- src/components/pokemon/pokemon-list.tsx
+++ src/components/pokemon/pokemon-list.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Pokemon } from '@/types/pokemon';
+import { PokemonDetails } from '@/types/pokemon';
 import { useTranslations } from 'next-intl';
 import { useInfinitePokemonQuery } from '@/lib/hooks/use-pokemon-queries';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
+import Image from 'next/image';
 import { TYPE_COLORS } from '@/lib/constants';
 
 export default function PokemonList() {
@@ -11,6 +12,11 @@ export default function PokemonList() {
 
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isLoading, error } =
     useInfinitePokemonQuery();
+
+  const capitalizeFirst = (str: string) => {
+    const fc = str.split('')[0].toUpperCase();
+    return fc + str.slice(1);
+  };
 
   if (isLoading) return <h1>{t('loading')}</h1>;
   if (error)
@@ -25,14 +31,24 @@ export default function PokemonList() {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4">
         {data?.pages.map((page, pageIndex) => (
           <React.Fragment key={pageIndex}>
-            {page.results.map((pokemon: Pokemon) => (
-              <Card key={pokemon.name}>
-                <CardHeader>
-                  <CardTitle>{pokemon.name}</CardTitle>
-                </CardHeader>
-                <CardContent>
-                  <p>More details coming soon!</p>
-                </CardContent>
+            {page.pokemonWithDetails?.map((pokemon: PokemonDetails) => (
+              <Card key={pokemon.name} className="grid grid-row-1 grid-cols-3 gap-2">
+                <div className="col-span-1 flex content-center justify-center p-2">
+                  <Image
+                    src={pokemon.sprites.other['official-artwork'].front_default}
+                    alt={pokemon.name}
+                    width={120}
+                    height={120}
+                  ></Image>
+                </div>
+                <div className="col-span-2">
+                  <CardHeader className="pl-2">
+                    <CardTitle>{capitalizeFirst(pokemon.name)}</CardTitle>
+                  </CardHeader>
+                  <CardContent className="pl-2">
+                    <p>More details coming soon!</p>
+                  </CardContent>
+                </div>
               </Card>
             ))}
           </React.Fragment>
