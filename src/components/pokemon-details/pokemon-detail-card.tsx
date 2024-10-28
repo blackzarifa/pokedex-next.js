5d28bed3026acba8a6a394ea97053fc871e0f45f
blackzarifa <rafaelirades@gmail.com>
2024-10-28 20:57:05 -0300

    feat: select sprite

--- src/components/pokemon-details/pokemon-detail-card.tsx
+++ src/components/pokemon-details/pokemon-detail-card.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, CardContent } from '@/components/ui/card';
 import PokemonImage from '@/components/pokemon-home/pokemon-image';
 import PokemonTypeBadge from '@/components/pokemon-home/pokemon-type-badge';
 import {
   Select,
+  SelectGroup,
+  SelectLabel,
   SelectContent,
   SelectItem,
   SelectTrigger,
@@ -15,14 +17,103 @@ interface PokemonDetailCardProps {
   pokemon: PokemonDetails;
 }
 
+type SpriteOption = {
+  value: string;
+  label: string;
+  src: string | null;
+};
+
 export default function PokemonDetailCard({ pokemon }: PokemonDetailCardProps) {
+  const [selectedSprite, setSelectedSprite] = useState<string>(
+    pokemon.sprites.other['official-artwork'].front_default
+  );
+
+  const createSpriteOptions = (): { [key: string]: SpriteOption[] } => {
+    const sprites = pokemon.sprites;
+
+    return {
+      'Official Artwork': [
+        {
+          value: 'official-default',
+          label: 'Default',
+          src: sprites.other['official-artwork'].front_default,
+        },
+        {
+          value: 'official-shiny',
+          label: 'Shiny',
+          src: sprites.other['official-artwork'].front_shiny,
+        },
+      ].filter((option) => option.src !== null),
+      'Front View': [
+        {
+          value: 'front-default',
+          label: 'Default',
+          src: sprites.front_default,
+        },
+        {
+          value: 'front-shiny',
+          label: 'Shiny',
+          src: sprites.front_shiny,
+        },
+        {
+          value: 'front-female',
+          label: 'Female',
+          src: sprites.front_female,
+        },
+        {
+          value: 'front-shiny-female',
+          label: 'Shiny Female',
+          src: sprites.front_shiny_female,
+        },
+      ].filter((option) => option.src !== null),
+      'Back View': [
+        {
+          value: 'back-default',
+          label: 'Default',
+          src: sprites.back_default,
+        },
+        {
+          value: 'back-shiny',
+          label: 'Shiny',
+          src: sprites.back_shiny,
+        },
+        {
+          value: 'back-female',
+          label: 'Female',
+          src: sprites.back_female,
+        },
+        {
+          value: 'back-shiny-female',
+          label: 'Shiny Female',
+          src: sprites.back_shiny_female,
+        },
+      ].filter((option) => option.src !== null),
+    };
+  };
+
+  const spriteOptions = createSpriteOptions();
+
+  const availableGroups = Object.entries(spriteOptions).filter(
+    ([_, options]) => options.length > 0
+  );
+
+  const handleSpriteChange = (value: string) => {
+    for (const [_, options] of Object.entries(spriteOptions)) {
+      const option = options.find((opt) => opt.value === value);
+      if (option?.src) {
+        setSelectedSprite(option.src);
+        break;
+      }
+    }
+  };
+
   return (
     <Card className="w-full h-full">
       <CardContent className="p-4 h-full flex flex-col">
         <div className="flex-grow flex items-center justify-center">
           <div className="w-full max-w-md aspect-square relative">
             <PokemonImage
-              src={pokemon.sprites.other['official-artwork'].front_default}
+              src={selectedSprite}
               cry={pokemon.cries.latest}
               alt={pokemon.name}
               isAnimating={false}
@@ -38,14 +129,21 @@ export default function PokemonDetailCard({ pokemon }: PokemonDetailCardProps) {
           </div>
 
           <div className="w-full max-w-xs mx-auto">
-            <Select disabled defaultValue="default">
+            <Select defaultValue="official-default" onValueChange={handleSpriteChange}>
               <SelectTrigger>
                 <SelectValue placeholder="Select sprite version" />
               </SelectTrigger>
               <SelectContent>
-                <SelectItem value="default">Default</SelectItem>
-                <SelectItem value="shiny">Shiny</SelectItem>
-                <SelectItem value="artwork">Official Artwork</SelectItem>
+                {availableGroups.map(([groupName, options]) => (
+                  <SelectGroup key={groupName}>
+                    <SelectLabel className="font-semibold">{groupName}</SelectLabel>
+                    {options.map((option) => (
+                      <SelectItem key={option.value} value={option.value}>
+                        {option.label}
+                      </SelectItem>
+                    ))}
+                  </SelectGroup>
+                ))}
               </SelectContent>
             </Select>
           </div>
