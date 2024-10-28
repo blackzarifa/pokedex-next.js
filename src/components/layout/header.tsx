4044b44083101f9b14402f6769eeef2d2d79d0a5
blackzarifa <rafaelirades@gmail.com>
2024-10-28 19:06:37 -0300

    feat: link on header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -5,6 +5,7 @@ import { ModeToggle } from '@/components/theme-toggle';
 import { LanguageSwitcher } from '@/components/language-switcher';
 import { Separator } from '@/components/ui/separator';
 import VolumeControl from '@/components/volume-control';
+import Link from 'next/link';
 
 export function Header() {
   const t = useTranslations('Home');
@@ -12,7 +13,9 @@ export function Header() {
   return (
     <header className="sticky top-0 z-50 w-full bg-background/95 backdrop-blur-xl supports-[backdrop-filter]:bg-background/60 mb-6">
       <div className="flex h-14 w-full items-center px-4">
-        <h1 className="text-xl font-bold">{t('title')}</h1>
+        <Link href="/pokemon">
+          <h1 className="text-xl font-bold">{t('title')}</h1>
+        </Link>
 
         <div className="flex flex-1 justify-end">
           <nav className="space-x-2">
