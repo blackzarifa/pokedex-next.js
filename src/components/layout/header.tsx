abe803c200ecb7b4de0f91337ba09a28f831f0a4
blackzarifa <rafaelirades@gmail.com>
2024-10-29 19:58:03 -0300

    feat: pokeball icon

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -6,6 +6,7 @@ import { LanguageSwitcher } from '@/components/language-switcher';
 import { Separator } from '@/components/ui/separator';
 import VolumeControl from '@/components/volume-control';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export function Header() {
   const t = useTranslations('Home');
@@ -13,7 +14,14 @@ export function Header() {
   return (
     <header className="sticky top-0 z-50 w-full bg-background/95 backdrop-blur-xl supports-[backdrop-filter]:bg-background/60 mb-6">
       <div className="flex h-14 w-full items-center px-4">
-        <Link href="/pokemon">
+        <Link href="/pokemon" className="flex items-center space-x-2">
+          <Image
+            src="/icons/pokeball.svg"
+            alt="Pokeball Icon"
+            width={26}
+            height={26}
+            aria-hidden="true"
+          />
           <h1 className="text-xl font-bold">{t('title')}</h1>
         </Link>
 
